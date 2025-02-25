4b2353882ed105dc6f2cdb40d50a317926a10dc9
f-hayyat <159626436+f-hayyat@users.noreply.github.com>
2025-02-25 15:35:58 +0500

    delete vercel

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,101 +1,132 @@
-import { useRef, useState } from 'react'
-import { useNavigate, Link } from 'react-router-dom';
-import ErrorMessages from '../common/ErrorMessages';
-import { useDispatch } from 'react-redux';
-import { login } from '../../store/slices/authSlice';
+import { useRef, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import ErrorMessages from "../common/ErrorMessages";
+import { useDispatch } from "react-redux";
+import { login } from "../../store/slices/authSlice";
 
 const Login = () => {
-  const [errorMessages, setErrorMessages] = useState([]);
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+	const [errorMessages, setErrorMessages] = useState([]);
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setErrorMessages([]);
-    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/auth/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: emailRef.current.value,
-        password: passwordRef.current.value
-      }),
-    });
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setErrorMessages([]);
+		const res = await fetch(
+			`${import.meta.env.VITE_API_BASE_URL}/api/auth/login`,
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					email: emailRef.current.value,
+					password: passwordRef.current.value,
+				}),
+			}
+		);
 
-    const data = await res.json();
-    console.log(data);
-    if (res.status === 200) {
-      await dispatch(login(data));
-      navigate("/");
-    } else if (res.status === 401) {
-      setErrorMessages(data.errorMessages)
-    }
-  }
+		const data = await res.json();
+		data;
+		if (res.status === 200) {
+			await dispatch(login(data));
+			navigate("/");
+		} else if (res.status === 401) {
+			setErrorMessages(data.errorMessages);
+		}
+	};
 
-  return (
-    <div className="max-w-md mx-auto p-6">
-      <h1 className="text-3xl font-bold text-gray-900 mb-8">Log in</h1>
-      <ErrorMessages errorMessages={errorMessages} />
-      
-      <form onSubmit={handleSubmit} className="space-y-6">
-        <div>
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-          <input
-            type="email"
-            id="email"
-            ref={emailRef}
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-2 py-1"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-          <div className="relative">
-            <input
-              type="password"
-              id="password"
-              ref={passwordRef}
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-2 py-1"
-              required
-            />
-            <button
-              type="button"
-              className="absolute inset-y-0 right-0 pr-3 flex items-center"
-              onClick={() => {
-                const passwordInput = document.getElementById('password');
-                if (passwordInput.type === 'password') {
-                  passwordInput.type = 'text';
-                } else {
-                  passwordInput.type = 'password';
-                }
-              }}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-red-700">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-              </svg>
-            </button>
-          </div>
-          <div className="text-right mt-1">
-            <Link to="/forgot-password" className="text-sm text-indigo-600 hover:text-indigo-500">
-              Forgot password?
-            </Link>
-          </div>
-        </div>
-        <div>
-          <button
-            type="submit"
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Log Me In
-          </button>
-        </div>
-      </form>
-    </div>
-  )
-}
+	return (
+		<div className="max-w-md mx-auto p-6">
+			<h1 className="text-3xl font-bold text-gray-900 mb-8">Log in</h1>
+			<ErrorMessages errorMessages={errorMessages} />
+
+			<form onSubmit={handleSubmit} className="space-y-6">
+				<div>
+					<label
+						htmlFor="email"
+						className="block text-sm font-medium text-gray-700"
+					>
+						Email
+					</label>
+					<input
+						type="email"
+						id="email"
+						ref={emailRef}
+						className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-2 py-1"
+						required
+					/>
+				</div>
+				<div>
+					<label
+						htmlFor="password"
+						className="block text-sm font-medium text-gray-700"
+					>
+						Password
+					</label>
+					<div className="relative">
+						<input
+							type="password"
+							id="password"
+							ref={passwordRef}
+							className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-2 py-1"
+							required
+						/>
+						<button
+							type="button"
+							className="absolute inset-y-0 right-0 pr-3 flex items-center"
+							onClick={() => {
+								const passwordInput = document.getElementById("password");
+								if (passwordInput.type === "password") {
+									passwordInput.type = "text";
+								} else {
+									passwordInput.type = "password";
+								}
+							}}
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className="w-5 h-5 text-red-700"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+								/>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+								/>
+							</svg>
+						</button>
+					</div>
+					<div className="text-right mt-1">
+						<Link
+							to="/forgot-password"
+							className="text-sm text-indigo-600 hover:text-indigo-500"
+						>
+							Forgot password?
+						</Link>
+					</div>
+				</div>
+				<div>
+					<button
+						type="submit"
+						className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+					>
+						Log Me In
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+};
 
 export default Login;
--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -1,36 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isLoggedIn: localStorage.getItem("token") ? true : false,
-  token: localStorage.getItem("token") || null,
-  userType: localStorage.getItem("userType") || null,
-  expiryTime: localStorage.getItem("expiryTime") || null,
+	isLoggedIn: localStorage.getItem("token") ? true : false,
+	token: localStorage.getItem("token") || null,
+	userType: localStorage.getItem("userType") || null,
+	expiryTime: localStorage.getItem("expiryTime") || null,
 };
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState: initialState,
-  reducers: {
-    login: (state, action) => {
-      console.log('state: ', state, 'action: ', action.payload);
-      state.isLoggedIn = true;
-      state.token = action.payload.token;
-      state.userType = action.payload.userType;
-      state.expiryTime = action.payload.expiryTime;
-      localStorage.setItem("token", action.payload.token);
-      localStorage.setItem("userType", action.payload.userType);
-      localStorage.setItem("expiryTime", action.payload.expiryTime);
-    },
-    logout: (state) => {
-      state.isLoggedIn = false;
-      state.token = null;
-      state.userType = null;
-      state.expiryTime = null;
-      localStorage.removeItem("token");
-      localStorage.removeItem("userType");
-      localStorage.removeItem("expiryTime");
-    }
-  }
+	name: "auth",
+	initialState: initialState,
+	reducers: {
+		login: (state, action) => {
+			"state: ", state, "action: ", action.payload;
+			state.isLoggedIn = true;
+			state.token = action.payload.token;
+			state.userType = action.payload.userType;
+			state.expiryTime = action.payload.expiryTime;
+			localStorage.setItem("token", action.payload.token);
+			localStorage.setItem("userType", action.payload.userType);
+			localStorage.setItem("expiryTime", action.payload.expiryTime);
+		},
+		logout: (state) => {
+			state.isLoggedIn = false;
+			state.token = null;
+			state.userType = null;
+			state.expiryTime = null;
+			localStorage.removeItem("token");
+			localStorage.removeItem("userType");
+			localStorage.removeItem("expiryTime");
+		},
+	},
 });
 
 export const { login, logout } = authSlice.actions;
