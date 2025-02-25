fc38ff8b29797fe200329ad641745a71b2f73fa1
f-hayyat <159626436+f-hayyat@users.noreply.github.com>
2025-02-25 18:58:32 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,6 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const corsOptions ={
- origin : process.env.FRONTEND_URL,
- methods: ["GET","POST","PUT","DELETE"],
- allowedHeaders: ["Content-Type" , "Authorization"]
-};
 // Local Module
 const errorController = require("./controllers/errorController");
 const sellerRouter = require("./routers/sellerRouter");
@@ -24,7 +19,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api/auth", authRouter);
 app.use("/api/seller", isLoggedIn, isSeller, sellerRouter);
